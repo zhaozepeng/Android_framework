9306c22fe02b1cde24d0080b1e5ff8c53661456e
zhaozepeng <zhao_zepeng@hotmail.com>
2015-07-08 11:39:26 +0800

    volley框架

--- libcore/src/main/java/com/android/libcore/netapi/BaseNetApi.java
+++ libcore/src/main/java/com/android/libcore/netapi/BaseNetApi.java
@@ -1,7 +1,9 @@
 package com.android.libcore.netapi;
 
 /**
- * Description: 所有网络访问基础类
+ * Description: 所有网络访问基础类，使用了volley框架，并且进行了简单的基础
+ * 封装，<font color=red><Strong>所有进行网络访问的地方都使用该封装类</Strong></font>，
+ * 方面以后修改网络访问框架
  *
  * @author zzp(zhao_zepeng@hotmail.com)
  * @since 2015-07-08
