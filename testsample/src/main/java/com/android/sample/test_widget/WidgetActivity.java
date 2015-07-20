2111bebfaa4150b08079d868baf06bbacf5e9658
zhaozepeng <zhao_zepeng@hotmail.com>
2015-07-20 15:29:31 +0800

    loading dialog完成40%

---
+++ testsample/src/main/java/com/android/sample/test_widget/WidgetActivity.java
@@ -0,0 +1,20 @@
+package com.android.sample.test_widget;
+
+import com.android.libcore_ui.activity.BaseActivity;
+
+/**
+ * Description: 测试导入控件的activity
+ *
+ * @author zzp(zhao_zepeng@hotmail.com)
+ * @since 2015-07-20
+ */
+public class WidgetActivity extends BaseActivity{
+    @Override
+    protected void initView() {
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
