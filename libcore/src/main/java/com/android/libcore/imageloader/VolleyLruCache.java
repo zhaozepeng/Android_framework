87a7e05c71e8e481128be3d8abbfaaa5f9699b40
zhaozepeng <zhao_zepeng@hotmail.com>
2015-10-21 18:11:54 +0800

    自定义imageloader完成20%

---
+++ libcore/src/main/java/com/android/libcore/imageloader/VolleyLruCache.java
@@ -0,0 +1,35 @@
+package com.android.libcore.imageloader;
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * @author zzp(zhao_zepeng@hotmail.com)
+ * @since 2015-10-20
+ */
+public class VolleyLruCache extends LruCache<String, Bitmap> implements ImageLoader.ImageCache{
+
+    private static int getCacheSize(){
+        return (int)(Runtime.getRuntime().maxMemory()/1024/8);
+    }
+
+    public VolleyLruCache() {
+        this(getCacheSize());
+    }
+
+    private VolleyLruCache(int size){
+        super(size);
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
