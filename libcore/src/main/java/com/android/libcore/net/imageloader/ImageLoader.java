f28fdbe8bc80b2710f1c1913f0c21ee904b4527e
zhaozepeng <zhao_zepeng@hotmail.com>
2015-10-23 10:11:30 +0800

    自定义imageloader完成80%

--- libcore/src/main/java/com/android/libcore/net/imageloader/ImageLoader.java
+++ libcore/src/main/java/com/android/libcore/net/imageloader/ImageLoader.java
@@ -40,44 +40,6 @@ public class ImageLoader {
         return instance;
     }
 
-    /**
-     * 代回调的加载图片
-     * @param url 图片url
-     * @param width
-     * @param height
-     * @param listener
-     */
-    public void loadImage(String url, int width, int height, final onLoadCallBack listener){
-        imageLoader.get(url, new com.android.volley.toolbox.ImageLoader.ImageListener() {
-            @Override
-            public void onResponse(com.android.volley.toolbox.ImageLoader.ImageContainer response, boolean isImmediate) {
-                listener.onLoadSuccess(response.getBitmap());
-            }
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                NetError netError = new NetError();
-                netError.transferVolleyError(error);
-                listener.onLoadFail(netError);
-            }
-        }, width, height);
-    }
-
-    /**
-     * 加载图片
-     * @param url 图片url
-     * @param imageView 需要加载该图片的url
-     * @param defaultImageResId 加载图片时的默认资源id
-     * @param errorImageResId 加载图片失败时显示的图片资源id
-     */
-    public void loadImage(String url, final ImageView imageView, int defaultImageResId,
-                          final int errorImageResId){
-        com.android.volley.toolbox.ImageLoader.ImageListener listener =
-                com.android.volley.toolbox.ImageLoader.getImageListener(imageView,
-                defaultImageResId, errorImageResId);
-        imageLoader.get(url, listener);
-    }
-
     /** 通过反射获取imageview的大小 */
     private int getImageViewFieldValue(Object object, String fieldName) {
         int value = 0;
@@ -95,10 +57,112 @@ public class ImageLoader {
     }
 
     /**
+     * 加载图片
+     * @param url 图片url
+     * @param imageView 需要加载图片的视图
+     */
+    public void loadImage(String url, final ImageView imageView){
+        loadImage(url, imageView, null);
+    }
+
+    /**
+     * 只带回调的图片加载
+     * @param url 图片url
+     * @param listener 图片加载回调
+     */
+    public void loadImage(String url, final onLoadCallBack listener){
+        loadImage(url, 0, 0, listener);
+    }
+
+    /**
+     * 带回调的加载图片
+     * @param url 图片url
+     * @param width 需要加载的图片宽
+     * @param height 需要加载的图片高
+     * @param listener 加载图片完成回调
+     */
+    public void loadImage(String url, int width, int height, final onLoadCallBack listener){
+        loadImage(url, null, width, height, listener);
+    }
+
+    /**
+     * 带回调的加载图片
+     * @param url 图片url
+     * @param imageView 需要加载图片的视图
+     * @param listener 加载图片的回调
+     */
+    public void loadImage(String url, final ImageView imageView, final onLoadCallBack listener){
+        int width = getImageViewFieldValue(imageView, "mMaxWidth");
+        int height = getImageViewFieldValue(imageView, "mMaxHeight");
+
+        loadImage(url, imageView, width, height, listener);
+    }
+
+    /**
+     * 加载图片
+     * @param url 图片url
+     * @param imageView 需要加载图片的视图
+     * @param width 需要加载视图的宽
+     * @param height 需要加载视图的高
+     * @param listener 加载图片回调
+     */
+    public void loadImage(String url, final ImageView imageView, int width, int height, final onLoadCallBack listener){
+        imageLoader.get(url, new com.android.volley.toolbox.ImageLoader.ImageListener() {
+            @Override
+            public void onResponse(com.android.volley.toolbox.ImageLoader.ImageContainer response, boolean isImmediate) {
+                if (imageView != null)
+                    imageView.setImageBitmap(response.getBitmap());
+                if (listener != null)
+                    listener.onLoadSuccess(response.getBitmap(), response.getRequestUrl());
+            }
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (listener != null) {
+                    NetError netError = new NetError();
+                    netError.transferVolleyError(error);
+                    listener.onLoadFail(netError);
+                }
+            }
+        }, width, height);
+    }
+
+    /**
+     * 加载图片
+     * @param url 图片url
+     * @param imageView 需要加载该图片的url
+     * @param defaultImageResId 加载图片时的默认资源id
+     * @param errorImageResId 加载图片失败时显示的图片资源id
+     */
+    public void loadImage(String url, final ImageView imageView, int defaultImageResId, int errorImageResId){
+        int width = getImageViewFieldValue(imageView, "mMaxWidth");
+        int height = getImageViewFieldValue(imageView, "mMaxHeight");
+
+        loadImage(url, imageView, defaultImageResId, errorImageResId, width, height);
+    }
+
+    /**
+     * 加载图片
+     * @param url 图片url
+     * @param imageView 需要加载该图片的url
+     * @param defaultImageResId 加载图片时的默认资源id
+     * @param errorImageResId 加载图片失败时显示的图片资源id
+     * @param width 加载图片的宽度
+     * @param height 加载图片的高度
+     */
+    public void loadImage(String url, final ImageView imageView, int defaultImageResId, int errorImageResId,
+                          int width, int height){
+        com.android.volley.toolbox.ImageLoader.ImageListener listener =
+                com.android.volley.toolbox.ImageLoader.getImageListener(imageView,
+                        defaultImageResId, errorImageResId);
+        imageLoader.get(url, listener, width, height);
+    }
+
+    /**
      * 加载图片回调
      */
     public interface onLoadCallBack{
-        void onLoadSuccess(Bitmap bitmap);
+        void onLoadSuccess(Bitmap bitmap, String url);
         void onLoadFail(NetError error);
     }
 }
