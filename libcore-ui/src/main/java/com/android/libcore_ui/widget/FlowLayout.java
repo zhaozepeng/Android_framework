a9c6eeb4c8d494ac3aac2bf18194710e3adc115b
zhaozepeng <zhao_zepeng@hotmail.com>
2015-11-17 16:35:55 +0800

    流式布局完成90%

--- libcore-ui/src/main/java/com/android/libcore_ui/widget/FlowLayout.java
+++ libcore-ui/src/main/java/com/android/libcore_ui/widget/FlowLayout.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.libcore.log.L;
 import com.android.libcore_ui.R;
 
 /**
@@ -18,10 +19,10 @@ public class FlowLayout extends ViewGroup{
     public static final int VERTICAL = 0;
     public static final int HORIZONTAL = 1;
     //默认间隙
-    int verticalSpacing = 10;
-    int horizontalSpacing = 10;
+    private int verticalSpacing = 10;
+    private int horizontalSpacing = 10;
     //布局方向
-    int orientation = HORIZONTAL;
+    private int orientation = HORIZONTAL;
 
     public FlowLayout(Context context) {
         this(context, null);
@@ -44,7 +45,7 @@ public class FlowLayout extends ViewGroup{
         orientation = typedArray.getInt(R.styleable.FlowLayout_orientation, HORIZONTAL);
 
         typedArray.recycle();
-        //TODO 1.变更orientation 2.修改子view布局gravity 3.编写博客，介绍关于flowlayout和attr的相关知识
+        //TODO  2.修改子view布局gravity 3.编写博客，介绍关于flowlayout和attr的相关知识
     }
 
     @Override
@@ -59,75 +60,119 @@ public class FlowLayout extends ViewGroup{
         int paddingRight = getPaddingRight();
         int paddingBottom = getPaddingBottom();
 
+        int width;
+        int height;
+        //水平布局，宽度固定，高度变化
         if (orientation == HORIZONTAL) {
-            int width = MeasureSpec.getSize(widthMeasureSpec);
-            int height = 0;
+            width = MeasureSpec.getSize(widthMeasureSpec);
+            height = 0;
+        }
+        //垂直布局，高度固定，宽度变化
+        else{
+            width = 0;
+            height = MeasureSpec.getSize(heightMeasureSpec);
+        }
 
-            int childWidth;
-            int childHeight;
+        int childWidth;
+        int childHeight;
 
-            //该行最大子view高度
-            int maxChildHeight = 0;
-            //剩余宽度
-            int lastWidth = width - paddingLeft - paddingRight;
+        //该行最大子view大小
+        int maxChildSize = 0;
+        //剩余大小
+        int lastSize;
+        if (orientation == HORIZONTAL) {
+            lastSize = width - paddingLeft - paddingRight;
+        }else{
+            lastSize = height - paddingTop - paddingBottom;
+        }
 
-            int x = paddingLeft;
-            int y = paddingTop;
+        int x = paddingLeft;
+        int y = paddingTop;
 
-            int childSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
-            for (int i = 0; i < getChildCount(); i++) {
-                View child = getChildAt(i);
+        int childSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
 
-                LayoutParams lp = (LayoutParams) child.getLayoutParams();
-                childHeight = lp.height;
-                childWidth = lp.width;
+            LayoutParams lp = (LayoutParams) child.getLayoutParams();
+            childHeight = lp.height;
+            childWidth = lp.width;
 
-                if (childHeight <= 0 || childWidth <= 0) {
-                    child.measure(childSpec, childSpec);
-                    childWidth = child.getMeasuredWidth();
-                    childHeight = child.getMeasuredHeight();
+            if (childHeight <= 0 || childWidth <= 0) {
+                child.measure(childSpec, childSpec);
+                childWidth = child.getMeasuredWidth();
+                childHeight = child.getMeasuredHeight();
+            }
+            child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.EXACTLY));
+
+            if (orientation == HORIZONTAL) {
+                lastSize = lastSize - childWidth - horizontalSpacing;
+            }else{
+                lastSize = lastSize - childHeight - verticalSpacing;
+            }
+
+            //如果第一个子view的大小已经超过容器大小
+            if (i==0 && lastSize <0) {
+                throw new ChildSizeTooLongException("the " + i + " child's width/height too long");
+            }
+
+            //需要换行
+            if (lastSize < 0) {
+                //将大小重置
+                if (orientation == HORIZONTAL) {
+                    lastSize = width - paddingLeft - paddingRight - childWidth;
+                }else{
+                    lastSize = height - paddingTop - paddingBottom - childHeight;
                 }
-                child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.EXACTLY),
-                        MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.EXACTLY));
-
-                lastWidth = lastWidth - childWidth - horizontalSpacing;
-                //如果第一个子view的宽度已经超过容器宽度
-                if (i==0 && lastWidth<0) {
-                    throw new ChildWidthTooLongException("the " + i + " child's width too long");
+                //换行之后该行的第一个view宽度超过整体父view宽度
+                if (lastSize < 0){
+                    throw new ChildSizeTooLongException("the " + i + " child's width/height too long");
                 }
 
-                //需要换行
-                if (lastWidth < 0) {
-                    //将宽度重置
-                    lastWidth = width - paddingLeft - paddingRight - childWidth;
-                    //换行之后该行的第一个view宽度超过整体父view宽度
-                    if (lastWidth < 0){
-                        throw new ChildWidthTooLongException("the " + i + " child's width too long");
-                    }
-                    //高度换行
-                    height += maxChildHeight + verticalSpacing;
+                if (orientation == HORIZONTAL) {
+                    //高换行
+                    height += maxChildSize + verticalSpacing;
                     //换行之后的第一行坐标
                     x = paddingLeft;
-                    y += maxChildHeight + verticalSpacing;
+                    y += maxChildSize + verticalSpacing;
                     //将最大高度值置为这第一个view的高度
-                    maxChildHeight = childHeight;
+                    maxChildSize = childHeight;
+                }else{
+                    //宽换列
+                    width += maxChildSize + horizontalSpacing;
+                    //换列之后的第一列坐标
+                    x += maxChildSize + horizontalSpacing;
+                    y = paddingTop;
+                    //将最大宽度值置为这第一个view的宽度
+                    maxChildSize = childWidth;
                 }
-                //不需要换行
-                else {
-                    //计算出这一行子view中高度最大的view
-                    maxChildHeight = maxChildHeight > childHeight ? maxChildHeight : childHeight;
-                }
-                lp.setXY(x, y);
-                x += childWidth + horizontalSpacing;
             }
-            height += maxChildHeight;
-            height -= verticalSpacing;
-            height = height + paddingBottom + paddingTop;
-            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
-            setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
-        }else{
-
+            //不需要换行
+            else {
+                if (orientation == HORIZONTAL) {
+                    //计算出这一行子view中高度最大的view
+                    maxChildSize = maxChildSize > childHeight ? maxChildSize : childHeight;
+                }else{
+                    //计算出这一列子view中宽度最大的view
+                    maxChildSize = maxChildSize > childWidth ? maxChildSize : childWidth;
+                }
+            }
+            lp.setXY(x, y);
+            if (orientation == HORIZONTAL) {
+                x += childWidth + horizontalSpacing;
+            }else{
+                y += childHeight + verticalSpacing;
+            }
         }
+        if (orientation == HORIZONTAL) {
+            height += maxChildSize;
+            height += + paddingBottom + paddingTop;
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        }else{
+            width += maxChildSize;
+            width += paddingLeft + paddingRight;
+        }
+        setMeasuredDimension(resolveSize(width, widthMeasureSpec), resolveSize(height, heightMeasureSpec));
     }
 
     @Override
@@ -149,6 +194,10 @@ public class FlowLayout extends ViewGroup{
             throw new IllegalArgumentException("orientation error");
         this.orientation = orientation;
         invalidate();
+    }
+
+    public int getOrientation(){
+        return orientation;
     }
 
     @Override
@@ -193,8 +242,8 @@ public class FlowLayout extends ViewGroup{
         }
     }
 
-    public static class ChildWidthTooLongException extends RuntimeException{
-        public ChildWidthTooLongException(String message){
+    public static class ChildSizeTooLongException extends RuntimeException{
+        public ChildSizeTooLongException(String message){
             super(message);
         }
     }
--- testsample/src/main/java/com/android/sample/test_widget/FlowLayoutActivity.java
+++ testsample/src/main/java/com/android/sample/test_widget/FlowLayoutActivity.java
@@ -1,6 +1,7 @@
 package com.android.sample.test_widget;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -10,18 +11,32 @@ import com.android.libcore_ui.activity.BaseActivity;
 import com.android.libcore_ui.widget.FlowLayout;
 
 /**
- * Description: #TODO
+ * Description:
  *
  * @author zzp(zhao_zepeng@hotmail.com)
  * @since 2015-11-14
  */
 public class FlowLayoutActivity extends BaseActivity{
+    int orientation = FlowLayout.HORIZONTAL;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentViewSrc(R.layout.activity_test_flow);
-        FlowLayout fl_content = (FlowLayout) findViewById(R.id.fl_content);
-        for (int i=0; i<50; i++){
+        findViewById(R.id.btn_change_orientation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                orientation = 1 - orientation;
+                if (orientation == FlowLayout.HORIZONTAL) {
+                    findViewById(R.id.sv_content).setVisibility(View.VISIBLE);
+                    findViewById(R.id.hsv_content).setVisibility(View.GONE);
+                } else {
+                    findViewById(R.id.sv_content).setVisibility(View.GONE);
+                    findViewById(R.id.hsv_content).setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        for (int i=0; i<20; i++){
             TextView view = new TextView(this);
             view.setTextColor(getResources().getColor(android.R.color.black));
             view.setTextSize(20);
@@ -32,10 +47,26 @@ public class FlowLayoutActivity extends BaseActivity{
             }
             int width = (int) (CommonUtils.dp2px(30) + Math.random()*300);
             int height = (int) (CommonUtils.dp2px(30) + Math.random()*300);
-            view.setText(CommonUtils.px2dp(width)+"dp");
+            view.setText(CommonUtils.px2dp(width) + "dp+"+CommonUtils.px2dp(height)+"dp");
             ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(width, height);
             view.setLayoutParams(lp);
-            fl_content.addView(view);
+            ((FlowLayout) findViewById(R.id.fl_horizontal)).addView(view);
+        }
+        for (int i=0; i<20; i++){
+            TextView view = new TextView(this);
+            view.setTextColor(getResources().getColor(android.R.color.black));
+            view.setTextSize(20);
+            if (i%2==0){
+                view.setBackgroundColor(getResources().getColor(R.color.darkorchid));
+            }else{
+                view.setBackgroundColor(getResources().getColor(R.color.palegreen));
+            }
+            int width = (int) (CommonUtils.dp2px(30) + Math.random()*300);
+            int height = (int) (CommonUtils.dp2px(30) + Math.random()*300);
+            view.setText(CommonUtils.px2dp(width) + "dp+"+CommonUtils.px2dp(height)+"dp");
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(width, height);
+            view.setLayoutParams(lp);
+            ((FlowLayout) findViewById(R.id.fl_vertical)).addView(view);
         }
     }
 }
